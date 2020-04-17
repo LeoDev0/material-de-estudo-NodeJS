3e8d112737408348946213a15bc923a9b45133e4
Leo <leo.80@protonmail.com>
2020-04-16 23:17:01 -0300

    'system-resources.js' exibe mais dados sobre a máquina

--- system-resources.js
+++ system-resources.js
@@ -3,19 +3,28 @@
 const os = require("os");
 
 setInterval(() => {
-  const { freemem, totalmem } = os;
+  const { freemem, totalmem, userInfo, uptime, type, hostname, arch } = os;
 
   const total = parseInt(totalmem() / 1024 / 1024);
   const mem = parseInt(freemem() / 1024 / 1024);
   const porcentagem = parseInt((mem / total) * 100);
+  const user_info = userInfo();
+  const time = uptime();
+  const OS = type();
+  const host = hostname();
+  const architecture = arch();
 
   const stats = {
     free: `${mem} MB`,
     total: `${total} MB`,
     usage: `${porcentagem}%`,
+    uptime: `${time} seconds`,
+    OS: `${OS}`,
+    hostname: `${host}`,
+    arch: `${architecture}`,
   };
 
   console.clear();
-  console.log("== System Resources ==");
+  console.log(" ===== System Resources ====");
   console.table(stats);
 }, 1000);
